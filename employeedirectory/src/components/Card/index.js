6933b82c900ee148e6a8239205cef4f0f9ed87a3
Margaret Elson <margaret.a.elson@gmail.com>
2021-03-30 17:06:12 -0400

    Still trying to get data to appear in the card file

--- employeedirectory/src/components/Card/index.js
+++ employeedirectory/src/components/Card/index.js
@@ -1,16 +1,32 @@
 import React from "react";
 import "./style.css";
+import App from "./App.js"
 
-function Card({name, picture, email}) {
-    return (
-        <div className="card">
-            <div className="img-container">
-                <img alt={name} src={picture} />
-            </div>
-            <p>{name}</p>
-            <p>{email}</p>
-        </div>
-    );
-}
+<App />
+
+
+    function Card (props) {
+        const [employeeState, setemployeeState] = useState([])
+
+        useEffect(function(){
+        API.search().then((result) => {
+            console.log(result.data.results)
+            setemployeeState(result.data.results)
+        }
+        )
+        },[]) 
+            
+        return (
+            employeeState.map(props => (
+                <div className="card">
+                    <div className="img-container">
+                        <img alt={props.firstname} src={props.picture} />
+                    </div>
+                    <p>{props.firstname}</p>
+                    <p>{props.email}</p>
+                </div>
+             ))
+           
+        )} 
 
 export default Card;
